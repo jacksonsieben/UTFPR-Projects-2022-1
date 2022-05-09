2fca14e43a596d1f61ce0a43343873b4ca5a50e2
sieben3105 <jackson_sieben@live.com>
2022-05-09 14:59:14 -0300

    ajust arvoreNaria

--- Estrutura de Dados/Arvores/ArvoreNaria.h
+++ Estrutura de Dados/Arvores/ArvoreNaria.h
@@ -1,11 +1,11 @@
 #ifndef ARVORE_NARIA_H
 #define ARVORE_NARIA_H
 
-/* implementacao de uma árvore n-ária de busca */
-#include "TAD_Arvore.h"
-
 /* observe que a arvore n-aria utiliza a biblioteca Lista.h */
 #include "ListaLinear/Lista.h"
+
+/* implementacao de uma ï¿½rvore n-ï¿½ria de busca */
+#include "TAD_Arvore.h"
 
 #include "Operacoes/ArvoreNaria/0_struct_Arvore.h"
 #include "Operacoes/0_struct_Descritor.h"
